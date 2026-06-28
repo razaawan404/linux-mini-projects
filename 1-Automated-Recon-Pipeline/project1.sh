87f034e22acac89ac1869f371dd27488a09c474e
razaawan404 <razaawan0372@gmail.com>
2026-06-28 21:02:16 +0500

    Validation under construction

--- 1-Automated-Recon-Pipeline/project1.sh
+++ 1-Automated-Recon-Pipeline/project1.sh
@@ -17,33 +17,66 @@ final_method(){
 validation(){
 
 	target="$1"
-	port="$2"
-	op_dir="$3"
+	port="$3"
+	op_dir="$5"
 
-	if [[ -z "$target" ]]; then
-        echo "input error: -t option missing for target ip"
-        exit 1
+	if [[ "$2" = true ]]; then
 
-        else if [[ -z "$port" ]]; then
-	echo "input error: -p option missing for port"
+		if [[ -z "$target" ]]; then
+        	echo "[!] input error: -t option missing for target ip"
+        	exit 1
+		fi
 
-	else if [[ -z "$
+		if [[ ! "$target" =~ ^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$ ]]; then
+	        echo "[!] ip error: enter valid ip address 255.255.255.0" 
+        	exit 1
+		fi
+	else
+		echo "[!] input error: you did'nt pass -t option value"
+		exit
+	fi
 
-        if [[ ! "$target" =~ ^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$ ]]; then
-        echo "ip error: enter valid ip address 255.255.255.0" 
-        exit 1
+	if [[ "$4" = true ]]; then
 
+		if [[ -z "$port" ]]; then
+		echo "[!] input error: -p option missing for port"
+		exit 1
+		fi
 
+		if [[ ! "$port" =~ ^[0-9]+$ ]]; then
+		echo "[!] input error: only integers are allowed for port!"
+		exit 1
+		fi
+
+		if [[ "$port" -lt 1 || "$port" -gt 65535 ]]; then 
+		echo "[!] range error: valid input range is (1 - 65535)"
+		exit 1
+		fi
+	else
+		echo "[!] input error: you did'nt pass -p option value"
+		exit 1
+	fi
 }
 
 
 while getopts "t:p:o:" opt
 do
         case $opt in
-                t) target="$OPTARG" ;;
+                t) 
+		target="$OPTARG"
+		target_given=true ;;
+		
+		p)
+		port="$OPTARG"
+		port_given=true ;;
+
+		o)
+		op_dir="$OPTARG"
+		op_dir_given=true ;;
          esac
 done
 
-validation "$1" "$2" "$3"
+	
+validation "$target" "$target_given" "$port" "$port_given" "$op_dir" "$op_dir_given"
 
 #final_method $target 
