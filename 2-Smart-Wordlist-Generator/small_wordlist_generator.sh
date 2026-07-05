f10aedcc84d56ee54e06d7476407091763c8615e
razaawan404 <razaawan0372@gmail.com>
2026-07-05 23:19:07 +0500

    Validation part 2

--- 2-Smart-Wordlist-Generator/small_wordlist_generator.sh
+++ 2-Smart-Wordlist-Generator/small_wordlist_generator.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+validation(){
+
+        if [[ "$1" = true ]]; then
+		if [[ -z "$2" ]]; then
+			echo "[!] Empty input! "
+		else
+			echo "[+] $2"
+		fi
+	fi
+}
+
 
 while getopts "t:o:w:" opt
 do
@@ -16,9 +27,5 @@ do
 done
 
 validation "$t_given" "$target" "$o_given" "$output_file" "$b_given" "$base"
-
-validation(){
-
-	if [[ "$1" = true ]]; then
-} 
+ 
 
