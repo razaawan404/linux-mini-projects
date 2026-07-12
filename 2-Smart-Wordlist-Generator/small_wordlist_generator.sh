cf16a29cd9a877c4c5c271c6accc59a7ed6be3b2
razaawan404 <razaawan0372@gmail.com>
2026-07-13 01:06:44 +0500

    Removing unnecessary words

--- 2-Smart-Wordlist-Generator/small_wordlist_generator.sh
+++ 2-Smart-Wordlist-Generator/small_wordlist_generator.sh
@@ -24,7 +24,7 @@ validation(){
 
 extraction(){
 
-	output=$(curl -s "$1" | sed -E 's/[</>"!-=0-9]/ /g; /style/,/style/d; /script/,/script/d' | grep -Eo '[a-zA-Z]+' | sort | uniq -u)
+	output=$(curl -s "$1" | sed -E 's/[</>"!-=0-9]/ /g; /style/,/style/d; /script/,/script/d' | grep -Eo '[a-zA-Z]+' | grep -v '(doctype|charset)' | sort | uniq -u)
 	echo "${output,,}"
 
 }
