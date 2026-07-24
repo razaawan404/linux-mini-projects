fac16c6d6e0af504fe85bfbbf1206e0b7863c6e3
razaawan404 <razaawan0372@gmail.com>
2026-07-24 22:23:35 +0500

    IP added

--- 4-Map-Your-Container-Using-proc/about_proc.sh
+++ 4-Map-Your-Container-Using-proc/about_proc.sh
@@ -5,3 +5,9 @@ echo "======================================"
 echo "	Container Map"
 echo "	Date: $(date +'%Y-%m-%d %H:%m')"
 echo "======================================"
+
+
+echo -e "\n[IP ADDRESS]"
+hostname -I | awk '{print $1}'
+
+echo -e "\n[LISTINING PORTS]"
