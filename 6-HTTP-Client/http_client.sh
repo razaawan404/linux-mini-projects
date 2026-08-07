30fe83bdcc96250d2006863f863e4be1ce36a3bd
razaawan404 <razaawan0372@gmail.com>
2026-08-08 01:52:19 +0500

    Request method developement

--- 6-HTTP-Client/http_client.sh
+++ 6-HTTP-Client/http_client.sh
@@ -2,22 +2,29 @@
 
 validate_flag_m(){
 
-	echo "$1"
+	if [[ ! "$1" =~ ^(GET|POST|get|post|Post|Get)$ ]]; then
+
+		echo "Error! $1 method does'nt exits"
+		exit 0
+	else
+
+		echo "Method: $1"
+	fi
 }
 validate_flag_u(){
 
 	echo "$1"
 }
 
-vlidate_flag_d(){
+validate_flag_d(){
 
 	echo "$1"
 }
 main(){
 
-	validate_flag_m
-	validate_flag_m
-	validate_flag_m
+	validate_flag_m "$1"
+	validate_flag_u "$2"
+	validate_flag_d "$3"
 
 }
 while getopts "m:u:d:" opts
@@ -34,3 +41,19 @@ do
 		   d_flag_given=true;;
 	esac
 done
+
+if [[ "$m_flag_given" != true ]]; then 
+
+	echo "./http_client.sh: option requires an argument -- m"
+fi
+
+if [[ "$u_flag_given" != true ]]; then
+
+	echo "./http_client.sh: option requires an argument -- u"
+fi
+if [[ "$d_flag_given" != true ]]; then
+
+	echo "./http_client.sh: option requires an argument -- d"
+fi
+
+main "$method" "$url" "$data"
