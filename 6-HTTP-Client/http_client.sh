2aeaa9c5506d4c7158daf2144d9581d745f860b5
razaawan404 <razaawan0372@gmail.com>
2026-08-07 23:57:22 +0500

    Arguments added

--- 6-HTTP-Client/http_client.sh
+++ 6-HTTP-Client/http_client.sh
@@ -1,5 +1,26 @@
 #!/usr/bin/env bash
 
 
-echo "HTTP client"
+main(){
 
+
+}
+while getopts "m:u:d:" opts
+do
+
+	case "$opts" in
+		m) method="$OPTARG"
+		   m_flag_given=true ;;
+
+		u) url="$OPTARG"
+		   u_flag_given=true;;
+
+		d) data="$OPTARG"
+		   d_flag_given=true;;
+	esac
+done
+
+
+echo "$method"
+echo "$url"
+echo "$data"
