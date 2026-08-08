f8536d0119b06cfe8b2c7f728ff39e2f875700b1
razaawan404 <razaawan0372@gmail.com>
2026-08-08 23:06:09 +0500

    Valiation Complete

--- 6-HTTP-Client/http_client.sh
+++ 6-HTTP-Client/http_client.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
 
+data=""
 validate_flag_m(){
 
 	if [[ ! "$1" =~ ^(GET|POST|get|post|Post|Get)$ ]]; then
 
 		echo "Error! $1 method does'nt exits"
 		exit 0
-	else
-
-		echo "Method: $1"
 	fi
 }
 validate_flag_u(){
 
-	echo "$1"
+	if [[ ! "$1" =~ ^https?://127.0.0.1 ]]; then
+
+		echo "Error! $1 url is not valid"
+		exit 0
+	fi
 }
 
 validate_flag_d(){
@@ -24,8 +26,6 @@ main(){
 
 	validate_flag_m "$1"
 	validate_flag_u "$2"
-	validate_flag_d "$3"
-
 }
 while getopts "m:u:d:" opts
 do
@@ -37,23 +37,11 @@ do
 		u) url="$OPTARG"
 		   u_flag_given=true;;
 
-		d) data="$OPTARG"
+		d) d="$OPTARG"
 		   d_flag_given=true;;
 	esac
 done
 
-if [[ "$m_flag_given" != true ]]; then 
-
-	echo "./http_client.sh: option requires an argument -- m"
-fi
-
-if [[ "$u_flag_given" != true ]]; then
-
-	echo "./http_client.sh: option requires an argument -- u"
-fi
-if [[ "$d_flag_given" != true ]]; then
-
-	echo "./http_client.sh: option requires an argument -- d"
-fi
-
-main "$method" "$url" "$data"
+"$data"="$d"
+echo "$data"
+main "$method" "$url"
