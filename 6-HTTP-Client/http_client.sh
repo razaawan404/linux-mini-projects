9d604a6e9a9c72d9a25560377b0d065fe17a4966
razaawan404 <razaawan0372@gmail.com>
2026-08-08 23:28:08 +0500

    Still working on validation

--- 6-HTTP-Client/http_client.sh
+++ 6-HTTP-Client/http_client.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-data=""
+v_dta=""
+v_url=""
+v_mtd=""
+
 validate_flag_m(){
 
 	if [[ ! "$1" =~ ^(GET|POST|get|post|Post|Get)$ ]]; then
@@ -8,6 +11,8 @@ validate_flag_m(){
 		echo "Error! $1 method does'nt exits"
 		exit 0
 	fi
+
+	echo "$1"
 }
 validate_flag_u(){
 
@@ -16,18 +21,29 @@ validate_flag_u(){
 		echo "Error! $1 url is not valid"
 		exit 0
 	fi
-}
 
+	echo "$1"
+}
 validate_flag_d(){
+
+	if [[ "user|pass" != "*$1*" ]]; then 
+
+		echo "Error! $1 invalid data, no user or pass added"
+		exit 0
+
+	fi
 
 	echo "$1"
 }
 main(){
 
-	validate_flag_m "$1"
-	validate_flag_u "$2"
+	v_mtd=$(validate_flag_m "$method")
+	v_url=$(validate_flag_u "$url")
+	v_dta=$(validate_flag_d "$data")
+
+	echo "$v_mtd"
 }
-while getopts "m:u:d:" opts
+while getopts ":m:u:d:" opts
 do
 
 	case "$opts" in
@@ -37,11 +53,26 @@ do
 		u) url="$OPTARG"
 		   u_flag_given=true;;
 
-		d) d="$OPTARG"
+		d) data="$OPTARG"
 		   d_flag_given=true;;
 	esac
 done
 
-"$data"="$d"
-echo "$data"
-main "$method" "$url"
+if [[ -z "$method" ]]; then
+
+	echo "./http_client: option requires an argument -- m"
+	exit 0
+fi
+
+if [[ -z "$url" ]]; then
+
+	echo "./http_client: option requires an argument -- u"
+	exit 0
+fi
+
+if [[ -z "$data" ]]; then
+
+	echo "./http_client: option requires an argument -- d"
+	exit 0
+fi
+main
