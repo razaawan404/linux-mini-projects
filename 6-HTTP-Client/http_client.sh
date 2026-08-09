05b3018906800baaea3de65d9b9b1b1e7e5bc894
razaawan404 <razaawan0372@gmail.com>
2026-08-09 23:28:01 +0500

    Single Quotes Added To Regex

--- 6-HTTP-Client/http_client.sh
+++ 6-HTTP-Client/http_client.sh
@@ -26,7 +26,7 @@ validate_flag_u(){
 }
 validate_flag_d(){
 
-	if [[ ! "$1" =~ ^'user=[a-zA-Z0-9]+&pass=[a-zA-Z0-9]+' ]]; then 
+	if [[ ! "$1" =~ '^user=[a-zA-Z0-9]+&pass=[a-zA-Z0-9]+$' ]]; then 
 
 		echo "Error! $1 invalid data, no user or pass added"
 		exit 0
