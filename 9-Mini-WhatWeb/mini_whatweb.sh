69460ed698dc95a05e5bc6d0af8180b6f56efa94
razaawan404 <razaawan0372@gmail.com>
2026-09-13 20:03:04 +0500

    updated the url regex for validation

--- 9-Mini-WhatWeb/mini_whatweb.sh
+++ 9-Mini-WhatWeb/mini_whatweb.sh
@@ -5,23 +5,23 @@ validate_url(){
 
 	url="$1"
 
-	if [[ ! "$url" =~ ^https?:\/\/[a-zA-Z0-9]+$ ]]; then
+	if [[ ! "$url" =~ ^https?://[a-zA-Z0-9.-]+(:[0-9]+)?(/.*)?$ ]]; then
 
 		echo "Invalid Url: Correct Format (http(s)//target.com)"
 		return 1
 	fi
-
+	echo "in validation"
 	echo "$url"
 }
 main(){
 
 	url="$1"
 
-	if v_url=$(validate_url "$url"); then
+	if ! v_url=$(validate_url "$url"); then
 		echo "$v_url"
 	fi	
 
-	echo "$v_url" 
+	echo "$v_url"	
 }
 while getopts ":u:" opts
 do
