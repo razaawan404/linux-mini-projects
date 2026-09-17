65ff24742895b4f4492b7ae2652db1f77dae735a
Raza Awan <razaawan0372@gmail.com>
2026-09-17 20:44:55 +0500

    Working on validation

--- 10-Log-Parser/log_parser.sh
+++ 10-Log-Parser/log_parser.sh
@@ -1,9 +1,42 @@
 #!/usr/bin/env bash
 
-while getopts ":f:t:" as opts
+validate_file(){
+
+
+}
+
+validate_type(){
+
+
+}
+main(){
+
+	file="$1"
+	type="$2"
+
+	if ! v_file=$(validate_file "$file"); then
+
+		echo "v_file"
+		exit 1
+
+	elif ! v_type=$(validate_type "$type"); then
+
+		echo "v_type"
+		exit 1
+	fi
+
+	echo "$v_file"
+	echo "$t_file" 
+
+}
+while getopts ":f:t:" opts
 do
 
 	case "$opts" in
 
 		f) log_file="$OPTARG" ;;
-		t) 
+		t) type="$OPTARG" ;;
+	esac
+done
+
+main "$log_file" "$type"
