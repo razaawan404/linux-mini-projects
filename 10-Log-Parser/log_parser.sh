0fafe49e833dcbc6b84f87795b432378785b6959
Raza Awan <razaawan0372@gmail.com>
2026-09-21 21:20:04 +0500

    Updated

--- 10-Log-Parser/log_parser.sh
+++ 10-Log-Parser/log_parser.sh
@@ -216,10 +216,16 @@ run_ssh(){
 ssh_extractions(){
 
 	file="$1"
-	echo "[FAILED LOGINS]"
 
-	account=$(cat "$file" | grep -Eio 'failed.*root|failed.*admin|failed.*user|failed.*ubuntu' | sort | uniq -c | awk '{printf "%-10s %s\n", $1, $5}' | sort -nr)
+	#failed login for every user
+	echo -e "\n[FAILED LOGINS]"
+	account=$(cat "$file" | grep -Eio 'failed.*root|failed.*admin|failed.*user|failed.*ubuntu' | sort | uniq -c | awk '{printf "%-7s %s\n", $1, $5}' | sort -nr)
 	echo "$account"
+
+	#most repeated ip
+	echo -e "\n[TOP ATTACKER IPs]"
+	most_rep_ips=$(cat "$file" | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+' | sort -n | uniq -c | sort -nr | head -n 3 | awk '{printf "%-7s %s\n", $1, $2}')
+	echo "$most_rep_ips"
 }
 while getopts ":f:t:" opts
 do
