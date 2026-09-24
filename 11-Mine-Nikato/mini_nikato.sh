eb45e21a01faa6b9d5f16330ba8bb406492673a2
Raza Awan <razaawan0372@gmail.com>
2026-09-24 21:38:28 +0500

    validation added

--- 11-Mine-Nikato/mini_nikato.sh
+++ 11-Mine-Nikato/mini_nikato.sh
@@ -1,5 +1,33 @@
 #!/usr/bin/env bash
 
+validation_url(){
+
+	url="$1"
+
+	if [[ -z "$url" ]]; then
+
+		echo "./mini_nikato: option requires an argument -- u"
+		return 1
+
+	elif [[ ! "$url" =~ ^https?://[a-zA-Z0-9._-]+(:[0-9]+)?(/.*)?$ ]]; then
+
+		echo "Invalid Url: Correct Format (http(s)//target.com)"
+		return 1
+	fi
+
+	echo "$url" 
+}
+main(){
+
+	url="$1"
+
+	if ! validation_url "$url"; then
+
+		echo "$url"
+	fi
+
+	echo "$url"
+}
 while getopts ":u:" opts;
 do
 	case "$opts" in 
@@ -8,4 +36,4 @@ do
 	esac
 done
 
-echo "$url"
+main "$url"
